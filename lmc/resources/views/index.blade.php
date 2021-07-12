2379ac77f09f0ebb5b847fbfe31394fe95e3cbd4
domingo79 <andria140779@yahoo.it>
2021-07-12 19:54:43 +0200

    bootstrap

--- lmc/resources/views/index.blade.php
+++ lmc/resources/views/index.blade.php
@@ -16,16 +16,36 @@
 </head>
 
 <body>
+    <header>
+        <div class="mx-auto" style="width: 600px;">
+            <h1>Page Laverel Model Controller</h1>
+        </div>
+    </header>
 
-    @foreach($movies as $movie)
-    <h2>Title: {{$movie->title}}</h2>
-    <h3>Original Title: {{$movie->original_title}}</h3>
-    <ul>
-        <li>Nazionality: {{$movie->nationality}}</li>
-        <li>Released on: {{$movie->date}}</li>
-        <li>Vote: {{$movie->vote}}</li>
-    </ul>
-    @endforeach
+    <main>
+        <div class="container">
+            <div class="row">
+                @foreach($movies as $movie)
+                <div class="col-sm-4">
+                    <div class="card m-3" style="width: 20rem;">
+                        <img style="height: 400px;" src="{{$movie->poster}}" class="card-img-top" alt="{{$movie->title}}">
+                        <div class="card-body">
+                            <h4 class="card-title">Title:{{$movie->title}}</h4>
+                            <h6 class="card-subtitle">Original Title: {{$movie->original_title}}</h6>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Nazionality: {{$movie->nationality}}</li>
+                                <li class="list-group-item">Released on: {{$movie->date}}</li>
+                                <li class="list-group-item">Vote: {{$movie->vote}} / 10</li>
+                            </ul>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+
+        </div>
+    </main>
 </body>
 
 </html>
